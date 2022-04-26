141e8fe51853d536f018bb1c2f3c3abfdbae9ab3
sss0a <sss0a@LAPTOP-AM2AFN1C>
2022-04-26 17:45:34 +0900

    04/26 이형철 오후 작업분

--- src/main/java/com/project/foodbalance/board/model/dao/BoardDao.java
+++ src/main/java/com/project/foodbalance/board/model/dao/BoardDao.java
@@ -12,15 +12,14 @@ import com.project.foodbalance.board.model.vo.Board;
 import com.project.foodbalance.common.Paging;
 import com.project.foodbalance.common.SearchDate;
 
+
 @Repository("boardDao")
-public class BoardDao implements Serializable {
+public class BoardDao implements Serializable{
 
-
-	private static final long serialVersionUID = 2129957075764586224L;
-
+	private static final long serialVersionUID = -2083362122348685373L;
 	@Autowired
 	private SqlSessionTemplate session;
-	
+
 	public ArrayList<Board> selectTop3() {
 		List<Board> list = session.selectList("boardMapper.selectTop3");
 		return (ArrayList<Board>)list;
@@ -30,18 +29,18 @@ public class BoardDao implements Serializable {
 	public int selectListCount() {
 		return session.selectOne("boardMapper.getListCount");
 	}
-
-	//해당 번호에 대한 게시글 상세 조회
-	public Board selectBoard(int board_no) {
-		return session.selectOne("boardMapper.selectBoard", board_no);
-	}
-
+	
 	//한 페이지에 출력할 게시글 조회
 	public ArrayList<Board> selectList(Paging page) {
 		List<Board> list = session.selectList("boardMapper.selectList", page);
 		return (ArrayList<Board>)list;
 	}
-
+	
+	//해당 번호에 대한 게시글 상세 조회
+	public Board selectBoard(int board_no) {
+		return session.selectOne("boardMapper.selectBoard", board_no);
+	}
+	
 	//상세보기시에 조회수 1증가 처리
 	public int updateAddReadcount(int board_no) {
 		return session.update("boardMapper.addReadCount", board_no);
@@ -61,22 +60,39 @@ public class BoardDao implements Serializable {
 	public int deleteBoard(Board board) {
 		return session.delete("boardMapper.deleteBoard", board);
 	}
-
-
-	public ArrayList<Board> selectSearchTitle(String keyword) {
-		List<Board> list = session.selectList("boardMapper.searchTitle", keyword);
-		return (ArrayList<Board>)list;
-	}
-
-	public ArrayList<Board> selectSearchWriter(String keyword) {
-		List<Board> list = session.selectList("boardMapper.searchWriter", keyword);
-		return (ArrayList<Board>)list;
-	}
-
-	public ArrayList<Board> selectSearchDate(SearchDate date) {
-		List<Board> list = session.selectList("boardMapper.searchDate", date);
-		return (ArrayList<Board>)list;
-	}
 	
 
+	//제목 검색 페이징
+	public ArrayList<Board> selectSearchTitle(Paging page) {
+		List<Board> list = session.selectList("boardMapper.searchTitle", page);
+		return (ArrayList<Board>)list;
+	}
+	//총 게시글 갯수 조회 (제목 검색)
+	public int searchTitleCount(String keyword) {	
+		return session.selectOne("boardMapper.searchTitleCount", keyword);
+	}
+	
+	
+	//작성자 검색 페이징
+	public ArrayList<Board> selectSearchWriter(Paging page) {
+		List<Board> list = session.selectList("boardMapper.searchWriter", page);
+		return (ArrayList<Board>)list;
+	}
+	//총 게시글 갯수 조회 (작성자 검색)
+	public int searchWriterCount(String keyword) {
+		return session.selectOne("boardMapper.searchWriterCount", keyword);
+	}
+	
+//	//날짜 검색 페이징
+	public ArrayList<Board> selectSearchDate(Paging page) {
+		List<Board> list = session.selectList("boardMapper.searchDate", page);
+		return (ArrayList<Board>)list;
+	}
+	//총 게시글 갯수 조회 (날짜 검색)
+	public int searchDateCount(SearchDate date) {			
+		return session.selectOne("boardMapper.searchDateCount", date);
+	}
+	
+	
+	
 }
